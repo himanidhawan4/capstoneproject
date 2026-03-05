3f4203b10573dc77a7f5d0386e8096992db5efd7
Himani Dhawan <trustedgroup.ad@gmail.com>
2026-03-05 19:51:13 +0530

    Wrap App component with Router and ErrorBoundary

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './app.js';
+import ErrorBoundary from './ErrorBoundary.js'; // Import the safety net
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App /> 
+    <ErrorBoundary> 
+      <Router>
+        <App />
+      </Router>
+    </ErrorBoundary>
   </React.StrictMode>
 );
